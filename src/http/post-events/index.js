7fe640353e82243e77171c6b797e925f21560db0
Aydrian Howard <aydrian@gmail.com>
2020-05-18 15:00:11 -0400

    added slack signing verification and url_verification response

--- src/http/post-events/index.js
+++ src/http/post-events/index.js
@@ -1,14 +1,60 @@
 // learn more about HTTP functions here: https://arc.codes/primitives/http
 const arc = require("@architect/functions");
+const crypto = require("crypto");
+const qs = require("qs");
+
+// fetch this from environment variables
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+const signVerification = async (req, res, next) => {
+  const slackSignature = req.headers["x-slack-signature"];
+  const requestBody = qs.stringify(req.body, { format: "RFC1738" });
+  const timestamp = req.headers["x-slack-request-timestamp"];
+
+  const time = Math.floor(new Date().getTime() / 1000);
+
+  if (Math.abs(time - timestamp) > 300) {
+    return res.status(400).send("Ignore this request.");
+  }
+
+  if (!slackSigningSecret) {
+    return res.status(400).send("Slack signing secret is empty.");
+  }
+
+  const sigBasestring = "v0:" + timestamp + ":" + requestBody;
+  const mySignature =
+    "v0=" +
+    crypto
+      .createHmac("sha256", slackSigningSecret)
+      .update(sigBasestring, "utf8")
+      .digest("hex");
+
+  if (
+    crypto.timingSafeEqual(
+      Buffer.from(mySignature, "utf8"),
+      Buffer.from(slackSignature, "utf8")
+    )
+  ) {
+    next();
+  } else {
+    return res.status(400).send("Verification failed");
+  }
+};
 
 const route = async function handler(req) {
   const { body } = req;
-
   console.log(body);
+
+  if (body.type === "url_verification") {
+    return {
+      headers: { "content-type": "application/json" },
+      status: 200,
+      body: JSON.stringify({ challenge: body.challenge }),
+    };
+  }
 
   return {
     status: 200,
   };
 };
 
-exports.handler = arc.http.async(route);
+exports.handler = arc.http.async(signVerification, route);
