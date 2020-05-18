8796ba386bfeb2f30e17be28298317b6fe4d075a
Aydrian Howard <aydrian@gmail.com>
2020-05-18 15:06:53 -0400

    fixed sign verification

--- src/http/post-events/index.js
+++ src/http/post-events/index.js
@@ -13,11 +13,17 @@ const signVerification = async (req, res, next) => {
   const time = Math.floor(new Date().getTime() / 1000);
 
   if (Math.abs(time - timestamp) > 300) {
-    return res.status(400).send("Ignore this request.");
+    return {
+      status: 400,
+      body: "Ignore this request.",
+    };
   }
 
   if (!slackSigningSecret) {
-    return res.status(400).send("Slack signing secret is empty.");
+    return {
+      status: 400,
+      body: "Slack signing secret is empty.",
+    };
   }
 
   const sigBasestring = "v0:" + timestamp + ":" + requestBody;
@@ -36,7 +42,10 @@ const signVerification = async (req, res, next) => {
   ) {
     next();
   } else {
-    return res.status(400).send("Verification failed");
+    return {
+      status: 400,
+      body: "Verification failed",
+    };
   }
 };
 
